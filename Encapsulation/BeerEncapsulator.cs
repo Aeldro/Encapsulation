a0de6569ca7f5191ef990fda6d96ec22ae323da7
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-17 11:28:56 +0200

    Ajoutez des fichiers projet.

---
+++ Encapsulation/BeerEncapsulator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class BeerEncapsulator
+    {
+        private double _avalaibleBeerVolume = -1;
+        private int _avalaibleBottles = -1;
+        private int _avalaibleCapsules = -1;
+
+        public int GetBottles() { return _avalaibleBottles; }
+        public int GetCapsules() { return _avalaibleCapsules; }
+        public double GetBeerVolume() { return _avalaibleBeerVolume; }
+        public void SetBottles(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("Le nombre de bouteilles ne peut pas être négatif.");
+            }
+            else
+            {
+                _avalaibleBottles = number;
+            }
+        }
+        public void SetCapsules(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("Le nombre de capsules ne peut pas être négatif.");
+            }
+            else
+            {
+                _avalaibleCapsules = number;
+            }
+        }
+
+        public void AddBeer(double quantity) { _avalaibleBeerVolume = _avalaibleBeerVolume + (quantity * 100); }
+        public int ProduceEncapsulatedBeerBottles(int number)
+        {
+            if (_avalaibleBottles < number)
+            {
+                Console.WriteLine($"Nombre de bouteilles insuffisant. Veuillez en rajouter au moins {number - _avalaibleBottles}.");
+                return 0;
+            }
+            else if (_avalaibleCapsules < number)
+            {
+                Console.WriteLine($"Nombre de capsules insuffisant. Veuillez en rajouter au moins {number - _avalaibleCapsules}.");
+                return 0;
+            }
+            else if (_avalaibleBeerVolume < 33 * number)
+            {
+                Console.WriteLine($"Quantité de bière insuffisante. Veuillez rajouter au moins {(number * 33) - (_avalaibleBeerVolume)}cl.");
+                return 0;
+            }
+            else
+            {
+                _avalaibleCapsules--;
+                _avalaibleBottles--;
+                _avalaibleBeerVolume = _avalaibleBeerVolume - (33 * number);
+                return number;
+            }
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                BeerEncapsulator beerEncapulator = new BeerEncapsulator();
+
+                while (beerEncapulator.GetBottles() < 0)
+                {
+                    Console.WriteLine("Combien de bouteilles sont disponibles?");
+                    beerEncapulator.SetBottles(Convert.ToInt32(Console.ReadLine()));
+                }
+
+                while (beerEncapulator.GetCapsules() < 0)
+                {
+                    Console.WriteLine("Combien de capsules sont disponibles?");
+                    beerEncapulator.SetCapsules(Convert.ToInt32(Console.ReadLine()));
+                }
+
+                while (beerEncapulator.GetBeerVolume() < 0)
+                {
+                    Console.WriteLine("Combien de litres de bières voulez-vous ajouter à la machine?");
+                    beerEncapulator.AddBeer(Convert.ToDouble(Console.ReadLine()));
+                }
+
+                Console.WriteLine("Combien de bouteilles voulez vous encapsuler?");
+                int encapsulation = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"{beerEncapulator.ProduceEncapsulatedBeerBottles(encapsulation)} bouteilles encapsulées.");
+            }
+        }
+    }
+}
